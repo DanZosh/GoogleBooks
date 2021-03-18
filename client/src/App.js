2ec168dde3a45f0aea5b8b2988009ed057a108ea
Danny Maier <dannymaier@gmail.com>
2021-03-17 18:09:39 -0700

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,19 @@ import React from "react";
 import "./_variables.sass"
 import "bootstrap/scss/bootstrap.scss"
 import GlobalNavbar from "./components/GlobalNavbar";
+import Header from "./components/Header";
+import {Container} from "react-bootstrap";
 
-// import "bootstrap/scss/_variables.scss"
-// import "bootstrap/scss/_mixins.scss"
-// import "bootstrap/scss/_functions.scss"
-// import "bootstrap/scss/_utilities.scss"
-// import "bootstrap/scss/bootstrap-reboot.scss"
-// import "bootstrap/scss/bootstrap-grid.scss"
-// import "bootstrap/scss/_navbar.scss"
+
 
 function App()  {
 
     return (
       <div>
         <GlobalNavbar/>
-        <div className="container">
-          <div className="row">
-
-          </div>
-        </div>
+        <Container className="py-5">
+          <Header/>
+        </Container>
       </div>
     );
   
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Jumbotron} from "react-bootstrap"
+
+function Header(){
+    return(
+    <Jumbotron>
+        <h1>My google Books</h1>
+        <p>
+            search and save google books.
+        </p>
+    </Jumbotron>
+    )
+}
+export default Header
